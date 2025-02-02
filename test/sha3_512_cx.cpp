aa1956339d5d5391ac112e55821bb20f86d6331e
Peter Dimov <pdimov@gmail.com>
2025-02-02 06:44:31 +0200

    Work around msvc-14.1 failures in sha3_384_cx and sha3_512_cx

--- test/sha3_512_cx.cpp
+++ test/sha3_512_cx.cpp
@@ -97,28 +97,36 @@ int main()
 
     {
         constexpr char const str1[] = "abc";
+
+        TEST_EQ( test<sha3_512>( 0, str1 ), digest_from_hex( "b751850b1a57168a5693cd924b6b096e08f621827444f70d884f5d0240d2712e10e116e9192af3c91a7ec57647e3934057340b4cf408d5a56592f8274eec53f0" ) );
+        TEST_EQ( test<sha3_512>( 7, str1 ), digest_from_hex( "eda4819a1d3ebaebdbd62dc1beb8db71230fb61c266b65f2a6f090d0d8aa45ad61ce906d75ac814b45bae4023246fcfac16cd46fd28e798b6c2599bcae6c924b" ) );
+
+#if !BOOST_WORKAROUND(BOOST_MSVC, < 1920)
+
         constexpr char const str2[] = "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu";
+
+        TEST_EQ( test<sha3_512>( 0, str2 ), digest_from_hex( "afebb2ef542e6579c50cad06d2e578f9f8dd6881d7dc824d26360feebf18a4fa73e3261122948efcfd492e74e82e2189ed0fb440d187f382270cb455f21dd185" ) );
+        TEST_EQ( test<sha3_512>( 7, str2 ), digest_from_hex( "e348824e6fc0c3c89208841431ca501dc1c36b7ce10324a67e7bee930278b9c200a5c281e166c0751fa1852ee19700bc163f1e63a51b9125236e0a415dae44f3" ) );
+
         constexpr unsigned char buf1[ 111 ] = {};
         constexpr unsigned char buf2[ 112 ] = {};
         constexpr unsigned char buf3[ 113 ] = {};
         constexpr unsigned char buf4[ 122 ] = {};
         constexpr unsigned char buf5[ 1000 ] = {};
 
-        TEST_EQ( test<sha3_512>( 0, str1 ), digest_from_hex( "b751850b1a57168a5693cd924b6b096e08f621827444f70d884f5d0240d2712e10e116e9192af3c91a7ec57647e3934057340b4cf408d5a56592f8274eec53f0" ) );
-        TEST_EQ( test<sha3_512>( 0, str2 ), digest_from_hex( "afebb2ef542e6579c50cad06d2e578f9f8dd6881d7dc824d26360feebf18a4fa73e3261122948efcfd492e74e82e2189ed0fb440d187f382270cb455f21dd185" ) );
         TEST_EQ( test<sha3_512>( 0, buf1 ), digest_from_hex( "c677adbab492fd76bff50e41bddae49b9c8fe0b47801a73a47632d8e895ccdc631e7f00f7f87fa5baec2ab86e47ea07072c829bdfe36f251e57d7f5507ff9d06" ) );
         TEST_EQ( test<sha3_512>( 0, buf2 ), digest_from_hex( "af8bd43e6f05532448f6509151a871db4ddd867f386aadb2b553a75e30f81bd3c2c79eec5cde15b0ef9be399ab166f4e1f2620934e584223d9f9504641a10f24" ) );
         TEST_EQ( test<sha3_512>( 0, buf3 ), digest_from_hex( "d501c1503b8ef0846ef4fef2a3383a98957296879a6047f3c9adea670666bdf8ba1c46283d99433bafab4df59f16da32fd66f1a43f7f4664b696635c92333e2c" ) );
         TEST_EQ( test<sha3_512>( 0, buf4 ), digest_from_hex( "017d18ab7042eacc42bcf9f100a2ba45a14594434e76b3e2c4c59bb7710b13e99d6388d2542d7f0318bb8e0467fe46f8a1277c6d26db59f09f28e46fa8b60815" ) );
         TEST_EQ( test<sha3_512>( 0, buf5 ), digest_from_hex( "422d49f19849352a41f4a61796feb28c18a79883fdb2cc52df63dc7cf0b13efddd757fac95861403b1cca3f28cb164f897df9bbfc2f579567d10f8fb21b5c6bf" ) );
 
-        TEST_EQ( test<sha3_512>( 7, str1 ), digest_from_hex( "eda4819a1d3ebaebdbd62dc1beb8db71230fb61c266b65f2a6f090d0d8aa45ad61ce906d75ac814b45bae4023246fcfac16cd46fd28e798b6c2599bcae6c924b" ) );
-        TEST_EQ( test<sha3_512>( 7, str2 ), digest_from_hex( "e348824e6fc0c3c89208841431ca501dc1c36b7ce10324a67e7bee930278b9c200a5c281e166c0751fa1852ee19700bc163f1e63a51b9125236e0a415dae44f3" ) );
         TEST_EQ( test<sha3_512>( 7, buf1 ), digest_from_hex( "124a2aae5e647bbd281a321032dcb21e76c0e45092bfbf798ff9bf17b963d4c189fcdd41bf59f7d19a509655cbc7974b72b97e99984100ef2c4042d952dc83af" ) );
         TEST_EQ( test<sha3_512>( 7, buf2 ), digest_from_hex( "c6dfb78a92d63a82fb57668a70ea480722879b06f310bcf04a00b771c92335a1461ee02ca94083e1405f92319f6206438c7e6ef1c2a28f6eabd2f4cfd9a12100" ) );
         TEST_EQ( test<sha3_512>( 7, buf3 ), digest_from_hex( "b5db27e2df1a9960d94811d1a2c79fb472a5d864edbc0312b6119fb63ec082be05dcbd1a66872b8049c5b34c0636242e1c29a4e3f625638682a6de31234a5df0" ) );
         TEST_EQ( test<sha3_512>( 7, buf4 ), digest_from_hex( "8b3d6c5596b989756b3c1339a5c4cac78a28c2cfc3913b635b63df77fac08a021d9cf6e9361d42511c568ecd8d14803a612b2c3f5937dc0e19bf566a480217d1" ) );
         TEST_EQ( test<sha3_512>( 7, buf5 ), digest_from_hex( "67548af9643ce72d83339b30fd18f50c2f1ca4806ce255eca7c314b7d7ad71dbb64bc0b162c79dba4a4b4643959dd06b0f34b23e6b931f778611bcbcfce86f44" ) );
+
+#endif
     }
 
     return boost::report_errors();
