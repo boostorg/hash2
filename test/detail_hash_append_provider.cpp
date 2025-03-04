7a498a8eb6dc5c2107b6a2a6d0eebf38be5501e4
Peter Dimov <pdimov@gmail.com>
2025-03-04 21:09:47 +0200

    Add test/detail_hash_append_provider.cpp

---
+++ test/detail_hash_append_provider.cpp
@@ -0,0 +1,74 @@
+// Copyright 2025 Peter Dimov
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include <boost/hash2/hash_append.hpp>
+#include <boost/hash2/fnv1a.hpp>
+#include <boost/core/lightweight_test.hpp>
+
+int main()
+{
+    unsigned const w[ 4 ] = { 1, 2, 3, 4 };
+
+    {
+        boost::hash2::detail::hash_append_provider pr;
+
+        boost::hash2::fnv1a_32 h1;
+        boost::hash2::hash_append( h1, {}, w );
+
+        boost::hash2::fnv1a_32 h2;
+        pr.hash_append( h2, {}, w );
+
+        BOOST_TEST_EQ( h1.result(), h2.result() );
+    }
+
+    {
+        boost::hash2::detail::hash_append_provider pr;
+
+        boost::hash2::fnv1a_32 h1;
+        boost::hash2::hash_append_range( h1, {}, w + 0, w + 4 );
+
+        boost::hash2::fnv1a_32 h2;
+        pr.hash_append_range( h2, {}, w + 0, w + 4 );
+
+        BOOST_TEST_EQ( h1.result(), h2.result() );
+    }
+
+    {
+        boost::hash2::detail::hash_append_provider pr;
+
+        boost::hash2::fnv1a_32 h1;
+        boost::hash2::hash_append_size( h1, {}, 4 );
+
+        boost::hash2::fnv1a_32 h2;
+        pr.hash_append_size( h2, {}, 4 );
+
+        BOOST_TEST_EQ( h1.result(), h2.result() );
+    }
+
+    {
+        boost::hash2::detail::hash_append_provider pr;
+
+        boost::hash2::fnv1a_32 h1;
+        boost::hash2::hash_append_range_and_size( h1, {}, w + 0, w + 4 );
+
+        boost::hash2::fnv1a_32 h2;
+        pr.hash_append_range_and_size( h2, {}, w + 0, w + 4 );
+
+        BOOST_TEST_EQ( h1.result(), h2.result() );
+    }
+
+    {
+        boost::hash2::detail::hash_append_provider pr;
+
+        boost::hash2::fnv1a_32 h1;
+        boost::hash2::hash_append_unordered_range( h1, {}, w + 0, w + 4 );
+
+        boost::hash2::fnv1a_32 h2;
+        pr.hash_append_unordered_range( h2, {}, w + 0, w + 4 );
+
+        BOOST_TEST_EQ( h1.result(), h2.result() );
+    }
+
+    return boost::report_errors();
+}
