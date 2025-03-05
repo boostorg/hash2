093f1de8d4f1f5c5e85a7f33d6d10054a1dc1bbd
Peter Dimov <pdimov@gmail.com>
2025-03-05 18:17:21 +0200

    Update test/get_integral_result_5.cpp

--- test/get_integral_result_5.cpp
+++ test/get_integral_result_5.cpp
@@ -3,29 +3,14 @@
 // https://www.boost.org/LICENSE_1_0.txt
 
 #include <boost/hash2/get_integral_result.hpp>
+#include <boost/hash2/fnv1a.hpp>
+#include <boost/hash2/hash_append.hpp>
 #include <boost/core/lightweight_test.hpp>
 #include <set>
 #include <limits>
 #include <cstdint>
 
-using T = std::uint32_t;
-
-void test_identity()
-{
-    using boost::hash2::get_integral_result;
-
-    using R = T;
-
-    for( unsigned i = 0; i < 65536; ++i )
-    {
-        R r = static_cast<R>( i * 0x9E3779B9u );
-        T t = get_integral_result<T>( r );
-
-        BOOST_TEST_EQ( t, r );
-    }
-}
-
-template<class R> void test_permutation( R offset = 0, R scale = 1 )
+template<class T, class Hash> void test_sample()
 {
     using boost::hash2::get_integral_result;
 
@@ -33,26 +18,12 @@ template<class R> void test_permutation( R offset = 0, R scale = 1 )
 
     for( unsigned i = 0; i < 65536; ++i )
     {
-        R r = static_cast<R>( i * 0x9E3779B9u * scale + offset );
-        T t = get_integral_result<T>( r );
+        T t1 = static_cast<T>( i );
 
-        dist.insert( t );
-    }
+        Hash h;
+        boost::hash2::hash_append( h, {}, t1 );
 
-    BOOST_TEST_EQ( dist.size(), 65536u );
-}
-
-template<class R> void test_roundtrip()
-{
-    using boost::hash2::get_integral_result;
-
-    std::set<T> dist;
-
-    for( unsigned i = 0; i < 65536; ++i )
-    {
-        T t1 = static_cast<T>( i * 0x9E3779B9u );
-        R r = get_integral_result<R>( t1 );
-        T t2 = get_integral_result<T>( r );
+        T t2 = get_integral_result<T>( h );
 
         dist.insert( t2 );
     }
@@ -62,13 +33,10 @@ template<class R> void test_roundtrip()
 
 int main()
 {
-    test_identity();
-
-    test_permutation<std::uint32_t>();
-    test_permutation<std::uint64_t>();
-
-    test_roundtrip<std::uint32_t>();
-    test_roundtrip<std::uint64_t>();
+    test_sample<std::uint32_t, boost::hash2::fnv1a_32>();
+    test_sample<std::uint32_t, boost::hash2::fnv1a_64>();
+    test_sample<std::uint64_t, boost::hash2::fnv1a_32>();
+    test_sample<std::uint64_t, boost::hash2::fnv1a_64>();
 
     return boost::report_errors();
 }
