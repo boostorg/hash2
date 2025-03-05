27592e9ad3cc1a67cd631fc638f038b4a0112a80
Peter Dimov <pdimov@gmail.com>
2025-03-05 17:19:12 +0200

    Update test/get_integral_result.cpp

--- test/get_integral_result.cpp
+++ test/get_integral_result.cpp
@@ -3,63 +3,100 @@
 // https://www.boost.org/LICENSE_1_0.txt
 
 #include <boost/hash2/get_integral_result.hpp>
+#include <boost/hash2/fnv1a.hpp>
+#include <boost/hash2/md5.hpp>
 #include <boost/hash2/digest.hpp>
 #include <boost/array.hpp>
 #include <array>
+#include <cstring>
 
-template<class R> void test()
+template<class Hash> void test()
 {
     using boost::hash2::get_integral_result;
 
-    R r = R();
+    Hash h;
 
-    get_integral_result<signed char>( r );
-    get_integral_result<unsigned char>( r );
-    get_integral_result<short>( r );
-    get_integral_result<unsigned short>( r );
-    get_integral_result<int>( r );
-    get_integral_result<unsigned int>( r );
-    get_integral_result<long>( r );
-    get_integral_result<unsigned long>( r );
-    get_integral_result<long long>( r );
-    get_integral_result<unsigned long long>( r );
+    get_integral_result<signed char>( h );
+    get_integral_result<unsigned char>( h );
+    get_integral_result<short>( h );
+    get_integral_result<unsigned short>( h );
+    get_integral_result<int>( h );
+    get_integral_result<unsigned int>( h );
+    get_integral_result<long>( h );
+    get_integral_result<unsigned long>( h );
+    get_integral_result<long long>( h );
+    get_integral_result<unsigned long long>( h );
 
-    get_integral_result<char>( r );
-    get_integral_result<char16_t>( r );
-    get_integral_result<char32_t>( r );
+    get_integral_result<char>( h );
+    get_integral_result<char16_t>( h );
+    get_integral_result<char32_t>( h );
+}
+
+template<class R> struct H1: private boost::hash2::fnv1a_64
+{
+    using result_type = R;
+
+    result_type result()
+    {
+        return static_cast<R>( boost::hash2::fnv1a_64::result() );
+    }
+};
+
+template<class R> struct H2: private boost::hash2::md5_128
+{
+    using result_type = R;
+
+    result_type result()
+    {
+        boost::hash2::md5_128::result_type r1 = boost::hash2::md5_128::result();
+        R r2 = {};
+
+        std::memcpy( &r2[0], &r1[0], std::min( r1.size(), r2.size() ) );
+        return r2;
+    }
+};
+
+template<class R> void test2()
+{
+    test< H1<R> >();
+}
+
+template<class R> void test3()
+{
+    test< H2<R> >();
 }
 
 int main()
 {
     using boost::hash2::get_integral_result;
 
-    test<unsigned char>();
-    test<unsigned short>();
-    test<unsigned int>();
-    test<unsigned long>();
-    test<unsigned long long>();
+    test2< unsigned char >();
+    test2< unsigned short >();
+    test2< unsigned int >();
+    test2< unsigned long >();
+    test2< unsigned long long >();
 
-    test< std::array<unsigned char, 8> >();
-    test< std::array<unsigned char, 16> >();
-    test< std::array<unsigned char, 20> >();
-    test< std::array<unsigned char, 28> >();
-    test< std::array<unsigned char, 32> >();
-    test< std::array<unsigned char, 48> >();
-    test< std::array<unsigned char, 64> >();
+    test3< std::array<unsigned char, 8> >();
+    test3< std::array<unsigned char, 16> >();
+    test3< std::array<unsigned char, 20> >();
+    test3< std::array<unsigned char, 28> >();
+    test3< std::array<unsigned char, 32> >();
+    test3< std::array<unsigned char, 48> >();
+    test3< std::array<unsigned char, 64> >();
 
-    test< boost::array<unsigned char, 8> >();
-    test< boost::array<unsigned char, 16> >();
-    test< boost::array<unsigned char, 20> >();
-    test< boost::array<unsigned char, 28> >();
-    test< boost::array<unsigned char, 32> >();
-    test< boost::array<unsigned char, 48> >();
-    test< boost::array<unsigned char, 64> >();
+    test3< boost::array<unsigned char, 8> >();
+    test3< boost::array<unsigned char, 16> >();
+    test3< boost::array<unsigned char, 20> >();
+    test3< boost::array<unsigned char, 28> >();
+    test3< boost::array<unsigned char, 32> >();
+    test3< boost::array<unsigned char, 48> >();
+    test3< boost::array<unsigned char, 64> >();
 
-    test< boost::hash2::digest<8> >();
-    test< boost::hash2::digest<16> >();
-    test< boost::hash2::digest<20> >();
-    test< boost::hash2::digest<28> >();
-    test< boost::hash2::digest<32> >();
-    test< boost::hash2::digest<48> >();
-    test< boost::hash2::digest<64> >();
+    test3< boost::hash2::digest<8> >();
+    test3< boost::hash2::digest<16> >();
+    test3< boost::hash2::digest<20> >();
+    test3< boost::hash2::digest<28> >();
+    test3< boost::hash2::digest<32> >();
+    test3< boost::hash2::digest<48> >();
+    test3< boost::hash2::digest<64> >();
 }
