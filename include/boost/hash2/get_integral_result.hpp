ccc9c08e6a89e8f13a54040fa64385b6129808bb
Peter Dimov <pdimov@gmail.com>
2025-03-07 03:55:37 +0200

    Remove unnecessary code from get_integral_result.hpp

--- include/boost/hash2/get_integral_result.hpp
+++ include/boost/hash2/get_integral_result.hpp
@@ -18,15 +18,6 @@ namespace hash2
 namespace detail
 {
 
-// identity
-
-template<class U, class R>
-    constexpr typename std::enable_if<sizeof(R) == sizeof(U), U>::type
-    get_result_multiplier()
-{
-    return 1;
-}
-
 // contraction
 
 // 2 -> 1
@@ -75,56 +66,6 @@ template<class U, class R>
     get_result_multiplier()
 {
     return 0xBFFFFFFF00000001ull;
-}
-
-// expansion
-
-// 1 -> 2
-template<class U, class R>
-    constexpr typename std::enable_if<sizeof(R) == 1 && sizeof(U) == 2, U>::type
-    get_result_multiplier()
-{
-    return 0x7F7F;
-}
-
-// 1 -> 4
-template<class U, class R>
-    constexpr typename std::enable_if<sizeof(R) == 1 && sizeof(U) == 4, U>::type
-    get_result_multiplier()
-{
-    return 0xBFBFBFBFu;
-}
-
-// 1 -> 8
-template<class U, class R>
-    constexpr typename std::enable_if<sizeof(R) == 1 && sizeof(U) == 8, U>::type
-    get_result_multiplier()
-{
-    return 0xDFDFDFDFDFDFDFDFull;
-}
-
-// 2 -> 4
-template<class U, class R>
-    constexpr typename std::enable_if<sizeof(R) == 2 && sizeof(U) == 4, U>::type
-    get_result_multiplier()
-{
-    return 0x7FFF7FFFu;
-}
-
-// 2 -> 8
-template<class U, class R>
-    constexpr typename std::enable_if<sizeof(R) == 2 && sizeof(U) == 8, U>::type
-    get_result_multiplier()
-{
-    return 0x7FFF7FFF7FFF7FFFull;
-}
-
-// 4 -> 8
-template<class U, class R>
-    constexpr typename std::enable_if<sizeof(R) == 4 && sizeof(U) == 8, U>::type
-    get_result_multiplier()
-{
-    return 0x7FFFFFFF7FFFFFFFull;
 }
 
 } // namespace detail
